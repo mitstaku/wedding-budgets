998fcdd764fc5f9b1c2c69594bb5de14cb70c360
mitstaku <takuya.mitsuhori@gmail.com>
2024-01-13 14:50:18 +0900

    微調整

--- app/controllers/event_quotes_controller.rb
+++ app/controllers/event_quotes_controller.rb
@@ -1,34 +1,32 @@
 class EventQuotesController < ApplicationController
+  # ユーザーがログインしていない場合、index以外のアクションは実行できないようにする
   before_action :authenticate_user!, except: [:index]
-  rescue_from ActiveRecord::RecordNotFound, with: :handle_record_not_found
 
+  # イベント見積もりの一覧を表示するアクション
   def index
+    # ログインしているユーザーのイベントを取得
     @events = user_signed_in? ? current_user.events : []
+    # ログインしているユーザーのイベント見積もりを取得
     @event_quotes = user_signed_in? ? EventQuote.where(event_id: current_user.events.pluck(:id)).group_by(&:event_id) : []
   end
 
+  # 新しいイベント見積もりを作成するアクション
   def new
+    # パラメータからイベントを取得
     @event = Event.find(params[:event_id])
+    # 新しいイベントフォームを作成
     @event_form = EventForm.new
-    @fieldIndices = {
-      foods: 1,
-      venues: 1,
-      ceremonies: 1,
-      costumes: 1,
-      beauties: 1,
-      flowers: 1,
-      prints: 1,
-      stagings: 1,
-      photos: 1,
-      videos: 1,
-      gifts: 1,
-      pre_ceremonies: 1
-    }
+    # 各フィールドのインデックスを初期化
+
   end
 
+  # イベント見積もりをデータベースに保存するアクション
   def create
+    # パラメータからイベントを取得
     @event = Event.find(params[:event_id])
+    # 新しいイベントフォームを作成し、パラメータを設定
     @event_form = EventForm.new(event_quote_params)
+    # イベントIDを設定
     @event_form.event_id = @event.id
 
     # 現在の日付をinput_dateに設定
@@ -40,105 +38,108 @@ class EventQuotesController < ApplicationController
     # 既存のイベントバージョンの数をカウントし、1を加えてversion_numberに設定
     @event_form.version_number = EventQuote.where(event_id: @event.id).count + 1
 
+    # フォームが有効な場合、保存してイベントページにリダイレクト
     if @event_form.valid?
       @event_form.save
       redirect_to event_path(@event)
     else
-      @fieldIndices = {
-        foods: 1,
-        venues: 1,
-        ceremonies: 1,
-        costumes: 1,
-        beauties: 1,
-        flowers: 1,
-        prints: 1,
-        stagings: 1,
-        photos: 1,
-        videos: 1,
-        gifts: 1,
-        pre_ceremonies: 1
-      }
       render :new
     end
   end
 
+  # イベント見積もりを削除するアクション
   def destroy
+    # パラメータからイベントとイベント見積もりを取得
     @event = Event.find(params[:event_id])
     @event_quote = @event.event_quotes.find(params[:id])
+    # イベントのユーザーIDが現在のユーザーIDと一致する場合、イベント見積もりを削除
     if @event.user_id == current_user.id
       @event_quote.destroy
       redirect_to event_path(@event)
     else
+      # 一致しない場合、イベントページにリダイレクト
       redirect_to event_path(@event)
     end
   end
 
+  # 新しい食事を作成するアクション
   def new_food
     @event_form = EventForm.new
     @event_form.foods.build
     render partial: 'food', locals: { index: Time.now.to_i, f: @event_form }
   end
 
+  # 新しい衣装を作成するアクション
   def new_costume
     @event_form = EventForm.new
     @event_form.costumes.build
     render partial: 'costume', locals: { index: Time.now.to_i, f: @event_form }
   end
 
+  # 新しい会場を作成するアクション
   def new_venue
     @event_form = EventForm.new
     @event_form.venues.build
     render partial: 'venue', locals: { index: Time.now.to_i, f: @event_form }
   end
 
+  # 新しい挙式を作成するアクション
   def new_ceremony
     @event_form = EventForm.new
     @event_form.ceremonies.build
     render partial: 'ceremony', locals: { index: Time.now.to_i, f: @event_form }
   end
 
+  # 新しい美容を作成するアクション
   def new_beauty
     @event_form = EventForm.new
     @event_form.beauties.build
     render partial: 'beauty', locals: { index: Time.now.to_i, f: @event_form }
   end
 
+  # 新しい花を作成するアクション
   def new_flower
     @event_form = EventForm.new
     @event_form.flowers.build
     render partial: 'flower', locals: { index: Time.now.to_i, f: @event_form }
   end
 
+  # 新しい印刷物を作成するアクション
   def new_print
     @event_form = EventForm.new
     @event_form.prints.build
     render partial: 'print', locals: { index: Time.now.to_i, f: @event_form }
   end
 
+  # 新しいステージングを作成するアクション
   def new_staging
     @event_form = EventForm.new
     @event_form.stagings.build
     render partial: 'staging', locals: { index: Time.now.to_i, f: @event_form }
   end
 
+  # 新しい写真を作成するアクション
   def new_photo
     @event_form = EventForm.new
     @event_form.photos.build
     render partial: 'photo', locals: { index: Time.now.to_i, f: @event_form }
   end
 
+  # 新しいビデオを作成するアクション
   def new_video
     @event_form = EventForm.new
     @event_form.videos.build
     render partial: 'video', locals: { index: Time.now.to_i, f: @event_form }
   end
 
+  # 新しいギフトを作成するアクション
   def new_gift
     @event_form = EventForm.new
     @event_form.gifts.build
     render partial: 'gift', locals: { index: Time.now.to_i, f: @event_form }
   end
 
+  # 新しい前挙式を作成するアクション
   def new_pre_ceremony
     @event_form = EventForm.new
     @event_form.pre_ceremonies.build
@@ -147,6 +148,7 @@ class EventQuotesController < ApplicationController
 
   private
 
+  # パラメータからイベントフォームのデータを取得するメソッド
   def event_quote_params
     params.require(:event_form).permit(
       :detail, :cost, :adult_count, :input_date, :child_count,
@@ -155,6 +157,7 @@ class EventQuotesController < ApplicationController
       costumes_attributes: [:id, :costume_type, :costume_cost],
       beauties_attributes: [:id, :beauty_type, :beauty_cost],
       flowers_attributes: [:id, :flower_type, :flower_cost],
+      venues_attributes: [:id, :venue_type, :venue_cost],
       prints_attributes: [:id, :print_type, :print_cost],
       stagings_attributes: [:id, :staging_type, :staging_cost],
       photos_attributes: [:id, :photo_type, :photo_cost],
@@ -162,10 +165,5 @@ class EventQuotesController < ApplicationController
       gifts_attributes: [:id, :gift_type, :gift_cost],
       pre_ceremonies_attributes: [:id, :pre_ceremony_type, :pre_ceremony_cost]
     ).merge(event_id: params[:event_id])
-  end
-
-  def handle_record_not_found
-    flash[:alert] = 'The event you were looking for could not be found.'
-    redirect_to root_path
   end
 end
--- app/models/event_form.rb
+++ app/models/event_form.rb
@@ -1,6 +1,7 @@
 class EventForm
   include ActiveModel::Model
 
+  # イベントフォームの属性を設定
   attr_accessor :event_id, :version_number, :detail, :cost, :input_date,
                 :ceremony_type, :ceremony_cost,
                 :adult_count, :child_count,
@@ -17,6 +18,7 @@ class EventForm
                 :pre_ceremony_type, :pre_ceremony_cost,
                 :foods, :ceremonies, :venues, :costumes, :beauties, :flowers, :prints, :stagings, :photos, :videos, :gifts, :pre_ceremonies
 
+  # バリデーションを設定
   validates :event_id, :version_number, :input_date,
             :adult_count, :child_count, presence: true
   validates :cost, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
@@ -24,9 +26,11 @@ class EventForm
   validates :ceremony_cost, :food_cost, :venue_cost, :costume_cost, :beauty_cost, :flower_cost, :print_cost, :staging_cost,
             :photo_cost, :video_cost, :gift_cost, :pre_ceremony_cost, numericality: { only_integer: true, allow_blank: true }
 
+  # イベントフォームの初期化
   def initialize(attributes = {})
-    super
+    super attributes
 
+    # 各属性が存在しない場合、新しいインスタンスを作成
     self.foods = 10.times.map { Food.new } unless foods.present?
     self.ceremonies = 10.times.map { Ceremony.new } unless ceremonies.present?
     self.venues = 10.times.map { Venue.new } unless venues.present?
@@ -41,6 +45,7 @@ class EventForm
     self.pre_ceremonies = 10.times.map { PreCeremony.new } unless pre_ceremonies.present?
   end
 
+  # 各属性のセッターメソッドを定義
   def foods_attributes=(attributes)
     self.foods = attributes.map { |_, v| Food.new(v) }
   end
@@ -89,20 +94,25 @@ class EventForm
     self.pre_ceremonies = attributes.map { |_, v| PreCeremony.new(v) }
   end
 
+  # イベントフォームの保存メソッド
   def save
+    # トランザクションを開始
     Event.transaction do
+      # イベント見積もりを作成
       event_quote = EventQuote.create!(
-        event_id:,
-        version_number:,
-        detail:,
-        cost:,
-        input_date:
+        event_id: event_id,
+        version_number: version_number,
+        detail: detail,
+        cost: cost,
+        input_date: input_date
       )
+      # ゲストを作成
       guest = Guest.create!(
         event_quote_id: event_quote.id,
-        adult_count:,
-        child_count:
+        adult_count: adult_count,
+        child_count: child_count
       )
+      # 各属性のインスタンスを作成
       ceremonies.each do |ceremony|
         Ceremony.create!(
           event_quote_id: event_quote.id,
@@ -187,64 +197,10 @@ class EventForm
           pre_ceremony_cost: pre_ceremony.pre_ceremony_cost
         )
       end
-
-      venue = Venue.create!(
-        event_quote_id: event_quote.id,
-        venue_type:,
-        venue_cost:
-      )
-      costume = Costume.create!(
-        event_quote_id: event_quote.id,
-        costume_type:,
-        costume_cost:
-      )
-      beauty = Beauty.create!(
-        event_quote_id: event_quote.id,
-        beauty_type:,
-        beauty_cost:
-      )
-      flower = Flower.create!(
-        event_quote_id: event_quote.id,
-        flower_type:,
-        flower_cost:
-      )
-      print = Print.create!(
-        event_quote_id: event_quote.id,
-        print_type:,
-        print_cost:
-      )
-      staging = Staging.create!(
-        event_quote_id: event_quote.id,
-        staging_type:,
-        staging_cost:
-      )
-      photo = Photo.create!(
-        event_quote_id: event_quote.id,
-        photo_type:,
-        photo_cost:
-      )
-      video = Video.create!(
-        event_quote_id: event_quote.id,
-        video_type:,
-        video_cost:
-      )
-      gift = Gift.create!(
-        event_quote_id: event_quote.id,
-        gift_type:,
-        gift_cost:
-      )
-      pre_ceremony = PreCeremony.create!(
-        event_quote_id: event_quote.id,
-        pre_ceremony_type:,
-        pre_ceremony_cost:
-      )
     end
     true
   rescue StandardError => e
+    # 例外が発生した場合、falseを返す
     false
-  end
-
-  def self.model_name
-    ActiveModel::Name.new(self, nil, 'EventForm')
   end
 end
